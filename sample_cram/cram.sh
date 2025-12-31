3d4fd0ec176114265821a68c076ce3b86b3d0176
justin_liao <justin_liao@gemteks.com>
2025-12-31 13:20:48 +0800

    remove kavia log

--- sample_cram/cram.sh
+++ sample_cram/cram.sh
@@ -24,7 +24,10 @@ ssh -o StrictHostKeychecking=no root@192.168.1.1 ls
 #run_check ssh root@$TARGET_DUT_LAN_IP 'logger -t CI -p local0.info "Testing starts, $(uptime -p)"'
 
 #Run All files in the folder
-run_check python3 -m cram --verbose ./sample_case/ | tee --append ./output/report_sample.txt
+#run_check python3 -m cram --verbose ./sample_case/ | tee --append ./output/report_sample.txt
+
+#run_check python3 -m cram --verbose ./sample_case/iptables/ | tee --append ./output/report_sample.txt
+
 
 #Run single script
-#run_check python3 -m cram --verbose ./sample_case/01-sample.t | tee --append ./output/report_sample.txt
+run_check python3 -m cram --verbose ./sample_case/03-sample3.t | tee --append ./output/report_sample.txt
